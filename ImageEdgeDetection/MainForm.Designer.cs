ac9e1c1d27635b51828ebe568d85b1f2afa22750
sachintha <sachintha.rajith@gmail.com>
2013-10-27 23:46:18 +0530

    Initial commit
    Signed-off-by: sachintha <sachintha.rajith@gmail.com>

---
+++ ImageEdgeDetection/MainForm.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace ImageEdgeDetection
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.picPreview = new System.Windows.Forms.PictureBox();
+            this.btnOpenOriginal = new System.Windows.Forms.Button();
+            this.btnSaveNewImage = new System.Windows.Forms.Button();
+            this.cmbEdgeDetection = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.picPreview)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // picPreview
+            // 
+            this.picPreview.BackColor = System.Drawing.Color.Silver;
+            this.picPreview.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.picPreview.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picPreview.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.picPreview.Location = new System.Drawing.Point(12, 135);
+            this.picPreview.Name = "picPreview";
+            this.picPreview.Size = new System.Drawing.Size(636, 539);
+            this.picPreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picPreview.TabIndex = 13;
+            this.picPreview.TabStop = false;
+            // 
+            // btnOpenOriginal
+            // 
+            this.btnOpenOriginal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpenOriginal.Location = new System.Drawing.Point(21, 16);
+            this.btnOpenOriginal.Name = "btnOpenOriginal";
+            this.btnOpenOriginal.Size = new System.Drawing.Size(126, 28);
+            this.btnOpenOriginal.TabIndex = 15;
+            this.btnOpenOriginal.Text = "Load Image";
+            this.btnOpenOriginal.UseVisualStyleBackColor = true;
+            this.btnOpenOriginal.Click += new System.EventHandler(this.btnOpenOriginal_Click);
+            // 
+            // btnSaveNewImage
+            // 
+            this.btnSaveNewImage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaveNewImage.Location = new System.Drawing.Point(522, 82);
+            this.btnSaveNewImage.Name = "btnSaveNewImage";
+            this.btnSaveNewImage.Size = new System.Drawing.Size(126, 32);
+            this.btnSaveNewImage.TabIndex = 16;
+            this.btnSaveNewImage.Text = "Save Image";
+            this.btnSaveNewImage.UseVisualStyleBackColor = true;
+            this.btnSaveNewImage.Click += new System.EventHandler(this.btnSaveNewImage_Click);
+            // 
+            // cmbEdgeDetection
+            // 
+            this.cmbEdgeDetection.BackColor = System.Drawing.SystemColors.Window;
+            this.cmbEdgeDetection.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbEdgeDetection.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbEdgeDetection.FormattingEnabled = true;
+            this.cmbEdgeDetection.Items.AddRange(new object[] {
+            "None",
+            "Sobel Operator",
+            "Prewitt Operator",
+            "Robert Operator",
+            "Canny Edge Ditection"});
+            this.cmbEdgeDetection.Location = new System.Drawing.Point(318, 14);
+            this.cmbEdgeDetection.Name = "cmbEdgeDetection";
+            this.cmbEdgeDetection.Size = new System.Drawing.Size(330, 32);
+            this.cmbEdgeDetection.TabIndex = 20;
+            this.cmbEdgeDetection.SelectedIndexChanged += new System.EventHandler(this.cmbEdgeDetection_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(187, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Select Operator";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(21, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 28);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "Do Operator";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(181, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Threshold";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(241, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Low";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(241, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "High";
+            this.label4.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(276, 77);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(43, 20);
+            this.textBox1.TabIndex = 26;
+            this.textBox1.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(274, 109);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(43, 20);
+            this.textBox2.TabIndex = 27;
+            this.textBox2.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(354, 77);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 13);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "Gaussian Size";
+            this.label5.Visible = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(438, 74);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(43, 20);
+            this.textBox3.TabIndex = 29;
+            this.textBox3.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(354, 109);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 13);
+            this.label6.TabIndex = 30;
+            this.label6.Text = "Gausian Sigma";
+            this.label6.Visible = false;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(438, 106);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(43, 20);
+            this.textBox4.TabIndex = 31;
+            this.textBox4.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(187, 58);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(461, 13);
+            this.label7.TabIndex = 32;
+            this.label7.Text = "If the values not specified default values will be assigned. High = 100, Low 50, " +
+                "Size = 5, Sigma 1";
+            this.label7.Visible = false;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel3,
+            this.toolStripProgressBar1,
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 674);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(670, 22);
+            this.statusStrip1.TabIndex = 33;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(94, 17);
+            this.toolStripStatusLabel1.Text = "Processing Time";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(77, 17);
+            this.toolStripStatusLabel2.Text = "HH:MM:SSSS";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(45, 17);
+            this.toolStripStatusLabel3.Text = "Status: ";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(670, 696);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbEdgeDetection);
+            this.Controls.Add(this.btnSaveNewImage);
+            this.Controls.Add(this.btnOpenOriginal);
+            this.Controls.Add(this.picPreview);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Image Edge Detection";
+            ((System.ComponentModel.ISupportInitialize)(this.picPreview)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picPreview;
+        private System.Windows.Forms.Button btnOpenOriginal;
+        private System.Windows.Forms.Button btnSaveNewImage;
+        private System.Windows.Forms.ComboBox cmbEdgeDetection;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+    }
+}
+
---
+++ ImageEdgeDetection/MainForm.cs
@@ -0,0 +1,230 @@
+﻿/*
+ * The Following Code was developed by Dewald Esterhuizen
+ * View Documentation at: http://softwarebydefault.com
+ * Licensed under Ms-PL 
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace ImageEdgeDetection
+{
+    public partial class MainForm : Form
+    {
+        private Bitmap originalBitmap = null;
+        private Bitmap previewBitmap = null;
+        private Bitmap resultBitmap = null;
+        
+        public MainForm()
+        {
+            InitializeComponent();
+
+            cmbEdgeDetection.SelectedIndex = 0;
+        }
+
+        private void btnOpenOriginal_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Select an image file.";
+            ofd.Filter = "Png Images(*.png)|*.png|Jpeg Images(*.jpg)|*.jpg";
+            ofd.Filter += "|Bitmap Images(*.bmp)|*.bmp";
+
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                StreamReader streamReader = new StreamReader(ofd.FileName);
+                originalBitmap = (Bitmap)Bitmap.FromStream(streamReader.BaseStream);
+                streamReader.Close();
+
+                previewBitmap = originalBitmap.CopyToSquareCanvas(picPreview.Width);
+                picPreview.Image = previewBitmap;
+
+                ApplyFilter(true);
+            }
+        }
+
+        private void btnSaveNewImage_Click(object sender, EventArgs e)
+        {
+            ApplyFilter(false);
+
+            if (resultBitmap != null)
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Title = "Specify a file name and file path";
+                sfd.Filter = "Png Images(*.png)|*.png|Jpeg Images(*.jpg)|*.jpg";
+                sfd.Filter += "|Bitmap Images(*.bmp)|*.bmp";
+
+                if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    string fileExtension = Path.GetExtension(sfd.FileName).ToUpper();
+                    ImageFormat imgFormat = ImageFormat.Png;
+
+                    if (fileExtension == "BMP")
+                    {
+                        imgFormat = ImageFormat.Bmp;
+                    }
+                    else if (fileExtension == "JPG")
+                    {
+                        imgFormat = ImageFormat.Jpeg;
+                    }
+
+                    StreamWriter streamWriter = new StreamWriter(sfd.FileName, false);
+                    resultBitmap.Save(streamWriter.BaseStream, imgFormat);
+                    streamWriter.Flush();
+                    streamWriter.Close();
+
+                    resultBitmap = null;
+                }
+            }
+        }
+
+        private void ApplyFilter(bool preview)
+        {
+            if (previewBitmap == null || cmbEdgeDetection.SelectedIndex == -1)
+            {
+                return;
+            }
+
+            Bitmap selectedSource = null;
+            Bitmap bitmapResult = null;
+
+            if (preview == true)
+            {
+                selectedSource = previewBitmap;
+            }
+            else
+            {
+                selectedSource = originalBitmap;
+            }
+
+            if (selectedSource != null)
+            {
+                if (cmbEdgeDetection.SelectedItem.ToString() == "None")
+                {
+                    bitmapResult = selectedSource;
+                }
+                else if (cmbEdgeDetection.SelectedItem.ToString() == "Sobel Operator")
+                {
+                    bitmapResult = selectedSource.Sobel3x3Filter();
+                }
+                else if (cmbEdgeDetection.SelectedItem.ToString() == "Prewitt Operator")
+                {
+                    bitmapResult = selectedSource.PrewittFilter();
+                }
+                else if (cmbEdgeDetection.SelectedItem.ToString() == "Robert Operator")
+                {
+                    bitmapResult = selectedSource.RobertFilter();
+                }
+                else if (cmbEdgeDetection.SelectedItem.ToString() == "Canny Edge Ditection")
+                {
+                    try
+                    {
+                        float TH = (float)Convert.ToDouble(textBox1.Text);
+                        float TL = (float)Convert.ToDouble(textBox2.Text);
+                        int MaskSize = Convert.ToInt32(textBox3.Text);
+                        float Sigma = (float)Convert.ToDouble(textBox4.Text);
+                        bitmapResult = selectedSource.CannyEdgeDitection(TH, TL, MaskSize, Sigma);
+                    }
+                    catch (Exception)
+                    {
+                        float TH = 52;
+                        float TL = 25;
+                        int MaskSize = 5;
+                        float Sigma = 1;
+                        bitmapResult = selectedSource.CannyEdgeDitection(TH, TL, MaskSize, Sigma);
+                    }
+                }
+            }
+
+            if (bitmapResult != null)
+            {
+                if (preview == true)
+                {
+                    picPreview.Image = bitmapResult;
+                }
+                else
+                {
+                    resultBitmap = bitmapResult;
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime dt1 = new DateTime();
+            DateTime dt2 = new DateTime();
+            TimeSpan dt3 = new TimeSpan();
+            dt1 = DateTime.Now;
+            toolStripProgressBar1.Value = 0;
+            ApplyFilter(true);
+            dt2 = DateTime.Now;
+            dt3 = dt2 - dt1;
+            toolStripStatusLabel2.Text = dt3.ToString();
+            toolStripProgressBar1.Value = 100;
+        }
+
+        private void indexChange(bool preview) {
+            if (cmbEdgeDetection.SelectedIndex == -1)
+            {
+                return;
+            }
+
+            Bitmap selectedSource = null;
+            Bitmap bitmapResult = null;
+
+            if (preview == true)
+            {
+                selectedSource = previewBitmap;
+            }
+            else
+            {
+                selectedSource = originalBitmap;
+            }
+            if (cmbEdgeDetection.SelectedItem.ToString() == "None")
+            {
+                bitmapResult = selectedSource;
+                ManageCannyOptions(false);
+            }
+            else if (cmbEdgeDetection.SelectedItem.ToString() == "Canny Edge Ditection")
+            {
+                ManageCannyOptions(true);
+            }
+            else {
+                ManageCannyOptions(false);
+            }
+            if (bitmapResult != null)
+            {
+                if (preview == true)
+                {
+                    picPreview.Image = bitmapResult;
+                }
+                else
+                {
+                    resultBitmap = bitmapResult;
+                }
+            }
+        }
+        private void cmbEdgeDetection_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            indexChange(true);
+        }
+
+        private void ManageCannyOptions(bool visibility) {
+            label2.Visible = visibility;
+            label3.Visible = visibility;
+            label4.Visible = visibility;
+            label5.Visible = visibility;
+            label6.Visible = visibility;
+            label7.Visible = visibility;
+            textBox1.Visible = visibility;
+            textBox2.Visible = visibility;
+            textBox3.Visible = visibility;
+            textBox4.Visible = visibility;
+        }
+    }
+}
